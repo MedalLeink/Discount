5619df2506ddfc136a5065f37c4484c36e4085af
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-11-12 10:52:22 -0500

    logic written for the day of the week

--- discount.py
+++ discount.py
@@ -5,19 +5,22 @@ SALES_TAX_RATE = .06   #6% Sales Tax
 
 discount = 0
 today = date.today() #Get today's date from computer's operating system
+day_of_the_week = today.weekday() #Return an int representing day of week (0 = Monday, 1 = Tuesday, ..., 7 = Sunday)
+
 
 subtotal = float(input("Enter the subtotal: $"))
+print("--------- Your Order --------")
+print(f"Subtotal: ${subtotal}")
 
-discount = subtotal * DISCOUNT_RATE
+if day_of_the_week == 2 or day_of_the_week ==3:
+    discount = subtotal * DISCOUNT_RATE
+    print(f"Discount Received: ${discount}")
 
 subtotal -= discount
 
 tax = subtotal * SALES_TAX_RATE
 
 total = subtotal + tax
-
-print("--------- Your Order --------")
-print(f"Subtotal: ${subtotal}")
 print(f"Discount Received: ${discount}")
 print(f"Sales Tax: ${tax}")
 print(f"Total Amount Due: ${total}")
