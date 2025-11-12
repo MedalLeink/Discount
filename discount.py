6dd9edd9e46ffeb22c067672d192e481eb40cb6e
Madeleine Nkiru <anaghamadeleine@gmail.com>
2025-11-12 10:46:02 -0500

    date importation and naming

--- discount.py
+++ discount.py
@@ -1,5 +1,10 @@
+from datetime import date
+
 DISCOUNT_RATE = .1     #10% Discount
 SALES_TAX_RATE = .06   #6% Sales Tax
+
+discount = 0
+today = date.today() #Get today's date from computer's operating system
 
 subtotal = float(input("Enter the subtotal: $"))
 
@@ -9,7 +14,7 @@ subtotal -= discount
 
 tax = subtotal * SALES_TAX_RATE
 
-total = subtotal * SALES_TAX_RATE
+total = subtotal + tax
 
 print("--------- Your Order --------")
 print(f"Subtotal: ${subtotal}")
